0ebc4a9967441a748f0bcf2a9cd14af902c7c143
Rahul <Rahul.Rajesh@hotmail.com>
2017-04-24 16:36:59 +0800

    Incorporated python script to convert csv files into database

--- mysite/dictionary/views.py
+++ mysite/dictionary/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 
 from .models import words_NASA
 
 def index(request):
 	word_list = words_NASA.objects.all()
-	return render(request, 'dictionary/home.html', context={'word_list': word_list})
+
+	query = request.GET.get("q")
+
+	if query:
+		word_list = word_list.filter(
+									Q(meaning=query)|
+									Q(acronym=query)
+									)
+		return render(request, 'dictionary/home.html', context={'word_list': word_list})
+
+	else:
+		return render(request, 'dictionary/home.html', context={'word_list': word_list})
 
